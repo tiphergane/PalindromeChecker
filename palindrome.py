23d9eaa430cf7d54731266163c4f56a00a031dd6
tiphergane <tiphergane@gmail.com>
2021-02-05 20:52:40 +0100

    Modification mineure de palindrome

--- palindrome.py
+++ palindrome.py
@@ -6,21 +6,21 @@ import pwn
 baseString = sys.argv[1]
 
 
-def reverseString(a):
+def reverseString(a):  # {{{
     x = str(a)[::-1]
     pwn.info("nous testons le mot {}".format(a))
     testString(x, baseString)
-    return
+    return  # }}}
 
 
-def testString(a, b):
+def testString(a, b):  # {{{
     src = a
     tgt = b
     if str(src) == str(tgt):
         pwn.success("{} est un palindrome".format(tgt))
     else:
         pwn.warn("{} n'est pas un palindrome".format(tgt))
-    return
+    return  # }}}
 
 
 reverseString(baseString)
